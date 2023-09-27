acd58cd2781465c8c8b616bea740de31addaa35b
Arian Nouri <nouri.arian@gmail.com>
2023-09-27 14:48:36 -0400

    Adding a barrier + randomized maze

--- #python.py
+++ #python.py
@@ -6,16 +6,38 @@ pygame.init()
 
 # Constants
 WIDTH, HEIGHT = 800, 600
-PLAYER_SIZE = 20
-TRAIL_LENGTH = 10  # Adjust the trail length as needed
+CELL_SIZE = 20
+GRID_WIDTH = WIDTH // CELL_SIZE
+GRID_HEIGHT = HEIGHT // CELL_SIZE
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+PLAYER_SIZE = 20
+TRAIL_LENGTH = 10
 PLAYER1_COLOR = (255, 0, 0)  # Red for Player 1
 PLAYER2_COLOR = (0, 0, 255)  # Blue for Player 2
 
 # Create the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Maze Racing Game")
+
+# Generate the maze using Recursive Backtracking algorithm
+def generate_maze():
+    grid = [[True] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
+
+    def recursive_backtracking(x, y):
+        directions = [(0, 2), (2, 0), (0, -2), (-2, 0)]
+        random.shuffle(directions)
+        for dx, dy in directions:
+            new_x, new_y = x + dx, y + dy
+            if 0 <= new_x < GRID_WIDTH and 0 <= new_y < GRID_HEIGHT and grid[new_y][new_x]:
+                grid[new_y][new_x] = False
+                grid[y + dy // 2][x + dx // 2] = False
+                recursive_backtracking(new_x, new_y)
+
+    recursive_backtracking(0, 0)
+    return grid
+
+maze = generate_maze()
 
 # Player positions and trail history
 player1_x, player1_y = 50, HEIGHT // 2
@@ -32,21 +54,21 @@ while running:
 
     # Handle player movements
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and player1_y > 0 and not maze[player1_y // CELL_SIZE - 1][player1_x // CELL_SIZE]:
         player1_y -= 5
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and player1_y < HEIGHT - PLAYER_SIZE and not maze[player1_y // CELL_SIZE + 1][player1_x // CELL_SIZE]:
         player1_y += 5
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and player1_x > 0 and not maze[player1_y // CELL_SIZE][player1_x // CELL_SIZE - 1]:
         player1_x -= 5
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] and player1_x < WIDTH - PLAYER_SIZE and not maze[player1_y // CELL_SIZE][player1_x // CELL_SIZE + 1]:
         player1_x += 5
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and player2_y > 0 and not maze[player2_y // CELL_SIZE - 1][player2_x // CELL_SIZE]:
         player2_y -= 5
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and player2_y < HEIGHT - PLAYER_SIZE and not maze[player2_y // CELL_SIZE + 1][player2_x // CELL_SIZE]:
         player2_y += 5
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and player2_x > 0 and not maze[player2_y // CELL_SIZE][player2_x // CELL_SIZE - 1]:
         player2_x -= 5
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and player2_x < WIDTH - PLAYER_SIZE and not maze[player2_y // CELL_SIZE][player2_x // CELL_SIZE + 1]:
         player2_x += 5
 
     # Add the current player positions to their respective trail
@@ -67,9 +89,12 @@ while running:
         print("Player 2 wins!")
         running = False
 
-    # Draw the maze (simplified here)
+    # Draw the maze
     screen.fill(WHITE)
-    pygame.draw.rect(screen, GREEN, (WIDTH // 2 - 5, 0, 10, HEIGHT))  # Maze center
+    for y in range(GRID_HEIGHT):
+        for x in range(GRID_WIDTH):
+            if maze[y][x]:
+                pygame.draw.rect(screen, GREEN, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
     pygame.draw.circle(screen, (0, 0, 255), (WIDTH // 2, HEIGHT // 2), 10)  # Center point
 
     # Draw the player trails
